e49bb0807f870f382b31e12634a9230e522380d2
Hexqueller <49871848+V1RUSnya@users.noreply.github.com>
2023-11-01 14:26:36 +0300

    backup

--- database.py
+++ database.py
@@ -1,32 +1,82 @@
+# -*- coding: cp1251 -*-
+import g4f
+import telebot
+from time import sleep
+from g4f.Provider import Yqcloud
 import sqlite3
+from database import BAZA
+import config
 
-class BAZA:
+g4f.debug.logging = True # enable logging
+g4f.check_version = False
+print(g4f.version)
 
-    def __init__(self, database):
-        self.connection = sqlite3.connect(database, check_same_thread=False)
-        self.cursor = self.connection.cursor()
+bot = telebot.TeleBot(config.token)
 
-    def get_subscriptions(self, status = True):
-        with self.connection:
-            return self.cursor.execute("SELECT * FROM `subscriptions` WHERE `status` = ?", (status,)).fetchall()
+baza = BAZA("data.db")
 
-    def subscriber_exists(self, user_id):
-        with self.connection:
-            result = self.cursor.execute('SELECT * FROM `subscriptions` WHERE `user_id` = ?', (user_id,)).fetchall()
-            return bool(len(result))
+def subs_check(messageID):
+    if (baza.subscriber_exists(messageID) and baza.subscriber_actual(messageID)):
+        A = True
+    else:
+        A = False
+    return A    
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+    bot.send_message(message.chat.id, "Добро пожаловать, {0.first_name}!\nЯ - <b>{1.first_name}</b>, бот с нейросетью gpt-4!\nЗадавайте любые вопросы!".format(message.from_user, bot.get_me()), parse_mode='html')
+    
+
+@bot.message_handler(commands=['subscribe'])
+def subs(message):
+    if (not baza.subscriber_exists(message.from_user.id)):
+        baza.add_subscriber(message.from_user.id, message.from_user.first_name)
+        bot.send_message(message.chat.id, "Вы успешно подписались!")
         
-    def subscriber_actual(self,user_id):
-        with self.connection:
-            result = self.cursor.execute('SELECT `status` FROM `subscriptions` WHERE `user_id` = ?', (user_id,)).fetchone()
-            return result[0]
-
-    def add_subscriber(self, user_id, username, status = True):
-        with self.connection:
-            return self.cursor.execute("INSERT INTO `subscriptions` (`user_id`, `name`, `status`) VALUES(?,?,?)", (user_id,username,status))
-
-    def update_subscription(self, user_id, status):
-        with self.connection:
-            return self.cursor.execute("UPDATE `subscriptions` SET `status` = ? WHERE `user_id` = ?", (status, user_id))
-
-    def close(self):
-        self.connection.close()
+    elif (baza.subscriber_exists(message.from_user.id) and not baza.subscriber_actual(message.from_user.id)):
+        baza.update_subscription(message.from_user.id, True)
+        bot.send_message(message.chat.id, "Вы снова подписаны!")
+    else:
+        baza.update_subscription(message.from_user.id, True)
+        bot.send_message(message.chat.id, "Вы уже подписались!")
+        
+@bot.message_handler(commands=['unsubscribe'])
+def subs(message):
+    if (baza.subscriber_exists(message.from_user.id) and baza.subscriber_actual(message.from_user.id)):
+        baza.update_subscription(message.from_user.id, False)
+        bot.send_message(message.chat.id, "Вы успешно отписались!")
+    else:
+        bot.send_message(message.chat.id, "Вы не подписаны!")
+    
+@bot.message_handler(content_types=['text'])
+def ask(message):
+    ps = "(Примечание: Отвечай на русском языке) "
+    if (subs_check(message.from_user.id)):
+        try:
+            print("\n\nВопрос: {0}, от пользователя {1.first_name}\n".format(message.text, message.from_user))
+            response = g4f.ChatCompletion.create(
+                model="gpt-3.5-turbo",
+                messages=[{"role": "user", "content": ps + message.text}],
+                provider= Yqcloud,
+                stream=True,
+            )
+            textCloud = ""
+            limit = 0
+            sent_message = bot.send_message(message.chat.id, "...") 
+            for messaga in response:
+                print(messaga, flush=True, end='')
+                textCloud += messaga
+                limit+=1
+                if limit == 4:
+                    limit = 0
+                    bot.edit_message_text(textCloud + "...", message.chat.id, sent_message.message_id)
+                    mem = textCloud
+            if textCloud != mem:        
+                bot.edit_message_text(textCloud, message.chat.id, sent_message.message_id) 
+        except telebot.apihelper.ApiTelegramException as e:
+            print(f"Ошибка: {e}! Ожидание 5 секунд")
+            sleep(5)
+    else:
+        bot.send_message(message.chat.id, "Пожалуйста зарегистрируйтесь! /subscribe")
+        
+bot.polling(none_stop=True)
